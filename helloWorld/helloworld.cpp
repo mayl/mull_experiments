56fe6557050b4690e3d40d30684620cad136a5e8
Larry May <larrymayster@gmail.com>
2021-10-31 16:07:58 -0400

    put example back to how it is in the tutorial

--- helloWorld/helloworld.cpp
+++ helloWorld/helloworld.cpp
@@ -1,7 +1,26 @@
 #define PROJECT_NAME "helloWorld"
 
-__attribute__((noinline))
-int valid_age(int age) {
+/* __attribute__((noinline)) */
+/* int valid_age(int age) { */
+/*     if (age >= 21) { */
+/*         return true; */
+/*     } */
+/*     return false; */
+/* } */
+
+/* int main() { */
+/*     volatile int test1 = valid_age(25) == true; */
+/*     if(!test1) return 1; */
+/*     volatile int test2 = valid_age(20) == false; */
+/*     if(!test2) return 1; */
+/*     int test3 = valid_age(21) == 1; */
+/*     if(!test3) return 1; */
+
+/*     // success */
+/*     return 0; */
+/* } */
+
+bool valid_age(int age) {
     if (age >= 21) {
         return true;
     }
@@ -9,13 +28,15 @@ int valid_age(int age) {
 }
 
 int main() {
-    volatile int test1 = valid_age(25) == true;
-    if(!test1) return 1;
-    volatile int test2 = valid_age(20) == false;
-    if(!test2) return 1;
-    /* int test3 = valid_age(21) == 1; */
-    /* if(!test3) return 1; */
+    int test1 = valid_age(25) == true;
+    if(!test1) {
+        return 1;
+    }
 
-    // success
+    int test2 = valid_age(20) == false;
+    if(!test2) {
+        return 1;
+    }
+
     return 0;
 }
